25b6949ff251f696b6b3025ee275a22722b1034e
YsH <mk.vyshnav1996@gmail.com>
2023-02-13 20:39:14 +0530

    feat: partial blog page

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,12 @@
 import React from 'react';
-import Tooltip, { EToolTipPosition } from './tooltip/Tooltip';
+import BlogPage from './component/BlogPage';
+
+
 
 const App = () => {
 	return (
 		<div>
-			<h1 className="text-3xl font-bold underline"> Hello world! </h1>
-			<Tooltip tooltipContent={{title: 'Konahamaru', body: 'Sarutobi ishji son Kona Hamaru'}} position={EToolTipPosition.TOP}>
-				<h1>Hello world!</h1>
-			</Tooltip>
-			<p>
-				Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia veniam
-				exercitationem animi illo nemo, ipsum nobis itaque sed ratione eos?
-				Expedita qui molestias voluptates delectus similique commodi aperiam
-				praesentium perspiciatis?
-			</p>
-			<Tooltip tooltipContent={{body: 'asdasdasd asdas asd'}} position={EToolTipPosition.TOP} >
-				<h1>Hello world2w! top</h1>
-			</Tooltip>
-			<br/>
-			<Tooltip tooltipContent={{body: 'asdasdasd asdas asd'}} position={EToolTipPosition.BOTTOM} >
-				<h1>Hello world2w! bottom</h1>
-			</Tooltip>
-			<br/>
-			<Tooltip tooltipContent={{body: 'asdasdasd asdas asd'}} position={EToolTipPosition.RIGHT} >
-				<h1>Hello world2w! right</h1>
-			</Tooltip>
-			<span>------------------------</span>
-			<Tooltip tooltipContent={{body: 'asdasdasd asdas asd'}} position={EToolTipPosition.LEFT} >
-				<h1>Hello world2w! l;eft 2</h1>
-			</Tooltip>
-			<span>------------------------</span>
-			<Tooltip tooltipContent={{body: 'asdasdasd asdas asd'}} position={EToolTipPosition.TOP} >
-				<h1>Hello world2w! l;top 2</h1>
-			</Tooltip>
-			<br/>
+			<BlogPage />
 		</div>
 	);
 };
---
+++ src/component/BlogPage.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import "./BlogPage.css"
+
+const LEFT_SIDEBAR_WIDTH = 25;
+const RIGHT_SIDEBAR_WIDTH = 25;
+const CONTENT_WIDTH = 50;
+
+const colorPalette = {
+	primary: '#bb86fc',
+	secondary: '#03dac5',
+	background: '#121212',
+	secondaryBackground: '#1e1e1e',
+	font: '#e1e1e1'
+}
+
+const containerStyle:React.CSSProperties = {background: colorPalette.background};
+const sidebarStyle:React.CSSProperties = {background: colorPalette.secondaryBackground};
+
+const BlogPage = () => {
+  return (
+    <div className='blog-page' style={containerStyle}>
+        <aside className='left-sidebar' style={sidebarStyle}> Left</aside>
+        <main className='main'> Main </main>
+        <aside className='right-sidebar' style={sidebarStyle}> Right </aside>
+    </div>
+  )
+}
+
+export default BlogPage
